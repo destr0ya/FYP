cf47a422e28855a9be59fbff331e1ffe067b6d10
Emma <emma.meehan3@hotmail.com>
2018-01-30 19:04:04 +0000

    Squat tracking working efficiently

--- App2/SkeletonPos.cs
+++ App2/SkeletonPos.cs
@@ -11,6 +11,7 @@ namespace App2
     class SkeletonPos
     {
         private KinectSensor sensor;
+        private static Skeleton skeleton;
 
         internal void StartSkeletonStream(KinectSensor sensor1)
         {
@@ -35,10 +36,14 @@ namespace App2
             {
                 foreach (Skeleton skeleton in skeletons)
                 {
-                    if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                    if (skeleton != null)
                     {
-                        SkeletonJointPrint(skeleton);
-                        CheckStandingPos(skeleton);
+                        if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            //SkeletonJointPrint(skeleton);
+                            //CheckStandingPos(skeleton);
+                            Squat.TrackSquat(skeleton);
+                        }
                     }
                 }
             }
@@ -58,7 +63,7 @@ namespace App2
                 }
         }
 
-        private void CheckStandingPos(Skeleton skeleton)
+        private void CheckStandingPos(Skeleton skeleton1)
         {
             //Checking if the skeleton is in a standing position, with head in line with neck, spine and bum,
             //with shoulder in line with elbow, wrist and hand and hip in line with knee, ankle and foot.
@@ -80,12 +85,15 @@ namespace App2
             Joint leftAnkle = skeleton.Joints[JointType.AnkleLeft];
             Joint rightAnkle = skeleton.Joints[JointType.AnkleRight];
 
+            skeleton = skeleton1;
+
             if (waist.TrackingState == JointTrackingState.NotTracked) return;
 
-            if (waist.Position.Z < 2.0f || waist.Position.Z > 4.5f)
+            if (waist.Position.Z < 2.0f || waist.Position.Z > 5.0f)
             {
                 return;
-            } else
+            }
+            else
             {
                 Debug.WriteLine("Upper X Margin: " + Math.Abs((head.Position.X + neck.Position.X + spine.Position.X + waist.Position.X) / 4 - Math.Abs(waist.Position.X)));
                 Debug.WriteLine("Upper Z Margin: " + Math.Abs((head.Position.Z + neck.Position.Z + spine.Position.Z + waist.Position.Z) / 4 - Math.Abs(waist.Position.Z)));
@@ -95,19 +103,19 @@ namespace App2
                 Debug.WriteLine("Right Arm Z Margin: " + Math.Abs((rightShoulder.Position.Z + rightElbow.Position.Z + rightHand.Position.Z) / 3 - Math.Abs(rightShoulder.Position.Z)));
                 Debug.WriteLine("Right Leg X Margin: " + Math.Abs((rightHip.Position.X + rightKnee.Position.X + rightAnkle.Position.X) / 3 - Math.Abs(rightHip.Position.X)));
                 Debug.WriteLine("Left Leg X Margin: " + Math.Abs((leftHip.Position.X + leftKnee.Position.X + leftAnkle.Position.X) / 3 - Math.Abs(leftHip.Position.X)));
-            }
 
-            if ((Math.Abs((head.Position.X + neck.Position.X + spine.Position.X + waist.Position.X)/4 - Math.Abs(waist.Position.X)) < 0.1f) && 
-                (Math.Abs((head.Position.Z + neck.Position.Z + spine.Position.Z + waist.Position.Z)/4 - Math.Abs(waist.Position.Z)) < 0.1f) && 
-                (Math.Abs((leftShoulder.Position.X + leftElbow.Position.X + leftHand.Position.X)/3 - Math.Abs(leftShoulder.Position.X)) < 0.1f) && 
-                (Math.Abs((leftShoulder.Position.Z + leftElbow.Position.Z + leftHand.Position.Z)/3 - Math.Abs(leftShoulder.Position.Z)) < 0.1f) &&
-                (Math.Abs((rightShoulder.Position.X + rightElbow.Position.X + rightHand.Position.X)/3 - Math.Abs(rightShoulder.Position.X)) < 0.1f) &&
-                (Math.Abs((rightShoulder.Position.Z + rightElbow.Position.Z + rightHand.Position.Z)/3 - Math.Abs(rightShoulder.Position.Z)) < 0.1f) &&
-                (Math.Abs((rightHip.Position.X + rightKnee.Position.X + rightAnkle.Position.X)/3 - Math.Abs(rightHip.Position.X)) < 0.1f) &&
-                (Math.Abs((leftHip.Position.X + leftKnee.Position.X + leftAnkle.Position.X) / 3 - Math.Abs(leftHip.Position.X)) < 0.1f))
-            {
-                Debug.WriteLine("STANDING POSITION FOUND WELL DONE");
-            } 
+                if ((Math.Abs((head.Position.X + neck.Position.X + spine.Position.X + waist.Position.X) / 4 - Math.Abs(waist.Position.X)) < 0.1f) &&
+                    (Math.Abs((head.Position.Z + neck.Position.Z + spine.Position.Z + waist.Position.Z) / 4 - Math.Abs(waist.Position.Z)) < 0.1f) &&
+                    (Math.Abs((leftShoulder.Position.X + leftElbow.Position.X + leftHand.Position.X) / 3 - Math.Abs(leftShoulder.Position.X)) < 0.1f) &&
+                    (Math.Abs((leftShoulder.Position.Z + leftElbow.Position.Z + leftHand.Position.Z) / 3 - Math.Abs(leftShoulder.Position.Z)) < 0.1f) &&
+                    (Math.Abs((rightShoulder.Position.X + rightElbow.Position.X + rightHand.Position.X) / 3 - Math.Abs(rightShoulder.Position.X)) < 0.1f) &&
+                    (Math.Abs((rightShoulder.Position.Z + rightElbow.Position.Z + rightHand.Position.Z) / 3 - Math.Abs(rightShoulder.Position.Z)) < 0.1f) &&
+                    (Math.Abs((rightHip.Position.X + rightKnee.Position.X + rightAnkle.Position.X) / 3 - Math.Abs(rightHip.Position.X)) < 0.1f) &&
+                    (Math.Abs((leftHip.Position.X + leftKnee.Position.X + leftAnkle.Position.X) / 3 - Math.Abs(leftHip.Position.X)) < 0.1f))
+                {
+                    Squat.TrackSquat(skeleton);
+                }
+            }
 
         }
     }
--- App2/Squat.cs
+++ App2/Squat.cs
@@ -8,12 +8,14 @@ using System.Drawing;
 using System.Windows.Media.Imaging;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Diagnostics;
 
 namespace App2
 {
     class Squat
     {
         private KinectSensor sensor;
+        Skeleton[] skeletons = new Skeleton[3];
 
         public ImageSource ShowSquatImage()
         {
@@ -26,25 +28,115 @@ namespace App2
         internal void StartSquatMode(KinectSensor sensor)
         {
             this.sensor = sensor;
-            SkeletonPos skeletonPos = new SkeletonPos();
-            skeletonPos.StartSkeletonStream(sensor);
-            //sensor.SkeletonStream.Enable(); // Enable skeletal tracking
-            
-            // Allocate ST data
-            
-            //sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinect_SkeletonFrameReady); // Get Ready for Skeleton Ready Events
+            StartSkeletonStream(sensor);
+        }
 
+        internal void StartSkeletonStream(KinectSensor sensor1)
+        {
+            this.sensor = sensor1;
+            sensor.SkeletonStream.Enable();
+            sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
             sensor.Start();
+        }
 
-            //Enter starting position
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+            if (skeletons.Length != 0)
+            {
+                foreach (Skeleton skeleton in skeletons)
+                {
+                    if (skeleton != null)
+                    {
+                        if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            if (CheckStartingPos(skeleton))
+                            {
+                                TrackSquat(skeleton);
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-            //When starting position entered, begin movement. 
+        internal Boolean CheckStartingPos(Skeleton skeleton)
+        {
+            Joint head = skeleton.Joints[JointType.Head];
+            Joint neck = skeleton.Joints[JointType.ShoulderCenter];
+            Joint rightShoulder = skeleton.Joints[JointType.ShoulderRight];
+            Joint leftShoulder = skeleton.Joints[JointType.ShoulderLeft];
+            Joint rightElbow = skeleton.Joints[JointType.ElbowRight];
+            Joint leftElbow = skeleton.Joints[JointType.ElbowLeft];
+            Joint rightHand = skeleton.Joints[JointType.WristRight];
+            Joint leftHand = skeleton.Joints[JointType.WristLeft];
+            Joint spine = skeleton.Joints[JointType.Spine];
+            Joint waist = skeleton.Joints[JointType.HipCenter];
+            Joint leftHip = skeleton.Joints[JointType.HipLeft];
+            Joint rightHip = skeleton.Joints[JointType.HipRight];
+            Joint leftKnee = skeleton.Joints[JointType.KneeLeft];
+            Joint rightKnee = skeleton.Joints[JointType.KneeRight];
+            Joint leftAnkle = skeleton.Joints[JointType.AnkleLeft];
+            Joint rightAnkle = skeleton.Joints[JointType.AnkleRight];
 
-            //When end position reached, stop recording. 
+            if ((Math.Abs((head.Position.X + neck.Position.X + spine.Position.X + waist.Position.X) / 4 - Math.Abs(waist.Position.X)) < 0.1f) &&
+                    (Math.Abs((head.Position.Z + neck.Position.Z + spine.Position.Z + waist.Position.Z) / 4 - Math.Abs(waist.Position.Z)) < 0.1f) &&
+                    (Math.Abs((leftShoulder.Position.Y + leftHand.Position.Y) / 2 - Math.Abs(leftShoulder.Position.Y)) < 0.1f) &&
+                    (Math.Abs((rightShoulder.Position.Y + rightHand.Position.Y) / 2 - Math.Abs(rightShoulder.Position.X)) < 0.1f) &&
+                    (Math.Abs((rightHip.Position.X + rightKnee.Position.X + rightAnkle.Position.X) / 3 - Math.Abs(rightHip.Position.X)) < 0.2f) &&
+                    (Math.Abs((leftHip.Position.X + leftKnee.Position.X + leftAnkle.Position.X) / 3 - Math.Abs(leftHip.Position.X)) < 0.2f))
+            {
+                return true;
+            }
+            else
+            {
+                return false; 
+            }
 
-            //Save as one gesture and analyse. 
+        }
 
-            //Average over 10 reps? 
+        internal static void TrackSquat(Skeleton skeleton)
+        {
+            if (skeleton.Joints[JointType.AnkleLeft].Position.Y > (skeleton.Joints[JointType.FootLeft].Position.Y + 0.05f))
+            {
+                Debug.WriteLine("Left heel coming off floor");
+            }
+
+            if (skeleton.Joints[JointType.AnkleRight].Position.Y > (skeleton.Joints[JointType.FootRight].Position.Y + 0.05f));
+            {
+                Debug.WriteLine("Right heel coming off floor");
+            }
+
+            if ((skeleton.Joints[JointType.Head].Position.Z < (skeleton.Joints[JointType.KneeLeft].Position.Z + 0.1f)) || 
+                (skeleton.Joints[JointType.Head].Position.Z < (skeleton.Joints[JointType.KneeRight].Position.Z + 0.1f))) {
+                Debug.WriteLine("Head coming too far forward");
+            }
+
+            if (skeleton.Joints[JointType.FootLeft].Position.X - Math.Abs(skeleton.Joints[JointType.HipCenter].Position.X) - 
+                skeleton.Joints[JointType.FootRight].Position.X - Math.Abs(skeleton.Joints[JointType.HipCenter].Position.X) > 0.1f)
+            {
+                Debug.WriteLine("Hips not in line");
+            }
+
+            if (skeleton.Joints[JointType.KneeLeft].Position.X < skeleton.Joints[JointType.AnkleLeft].Position.X - 0.05f)
+            {
+                Debug.WriteLine("Left knee out of line");
+            }
+
+            if (skeleton.Joints[JointType.KneeRight].Position.X > skeleton.Joints[JointType.AnkleRight].Position.X - 0.05f)
+            {
+                Debug.WriteLine("Right knee out of line");
+            }
+
+           //If depth isn't achieved
+
         }
 
     }
